953ae6695891feee2a39519e793e153410263190
Dmitry <example@gmail.com>
2022-09-22 21:16:05 +0300

    first commit

---
+++ js/js.js
@@ -0,0 +1,69 @@
+"use strict";
+
+//Firs lessons
+// console.log('Hi');
+
+// let obj = {
+//     name:'Alex',
+//     age: 10,
+// }
+
+// obj.name = 'John';
+// console.log(obj);
+
+//Second
+// let douBle = 4.6;
+// console.log(4/0);
+// console.log('string' * 9);
+
+// let und
+// console.log(und);
+
+// const secondObj ={
+//     name: "John",
+//     age: "25",
+//     isMarried: false
+// }
+// //console.log(obj["name"]);
+
+// let arr = [
+//     'plum.png', 'orange.jpg', 6, 'apple.bmp',
+// ]
+// console.log(arr[3]);
+
+// const answer = prompt("вопрос с полем");
+// const answer2 = confirm("вопрос с полем");
+// const answer3 = prompt("вопрос с полем");
+
+// const answ4 = [];
+// answ4[0] = prompt("вопрос с полем");
+// answ4[1] = prompt("вопрос с полем");
+// answ4[3] = prompt("вопрос с полем");
+
+
+// Five lessons
+// const category = 'toys';
+// const user = "Dmitry";
+// console.log(`https:/someurl.com${category}/5`);
+// alert(`user ${user}`);
+
+
+//Six les
+//console.log('arr' + "- obj");
+console.log(4 + "5");
+
+let incr= 10,
+    decr = 10;
+
+// incr ++; // ПостПрефексный
+// decr --;
+
+// ++incr;// Префексный
+// --decr;
+
+console.log(incr++);
+console.log(decr--);
+console.log(++incr);
+console.log(--decr);
+
+
